16c8f5bcd2f53aa088aab0b9719f28696075e5e4
Snehdeep Singh <snehdeepsingh221199@gmail.com>
2022-04-10 18:49:15 +0530

    rest api for Goals

--- backend/controllers/goalController.js
+++ backend/controllers/goalController.js
@@ -3,11 +3,14 @@
 // if we do not want to use try catch and just want to use error handler, we can use a package called express-async-handler
 const asyncHandler = require('express-async-handler')
 
+const Goal = require('../models/goalModel')
+
 // @desc    Get goals
 // @route   GET /api/goals
 // @access  Private
 const getGoals = asyncHandler(async (req, res) => {
-    res.status(200).json({message: 'Get goals'})
+    const goals = await Goal.find()
+    res.status(200).json(goals)
 })
 
 
@@ -15,11 +18,16 @@ const getGoals = asyncHandler(async (req, res) => {
 // @route   POST /api/goals
 // @access  Private
 const setGoal = asyncHandler(async (req, res) => {
-    if(!req.body.text) {
+    if (!req.body.text) {
         res.status(400)
         throw new Error('Please add a text field.')
     }
-    res.status(200).json({message: 'Set goal'})
+
+    const goal = await Goal.create({
+        text: req.body.text,
+    })
+
+    res.status(200).json(goal)
 })
 
 
@@ -27,7 +35,18 @@ const setGoal = asyncHandler(async (req, res) => {
 // @route   PUT /api/goals/:id
 // @access  Private
 const updateGoal = asyncHandler(async (req, res) => {
-    res.status(200).json({message: `Update goal ${req.params.id}`})
+    const goal = await Goal.findById(req.params.id)
+
+    if (!goal) {
+        res.status(400)
+        throw new Error('Goal not found')
+    }
+
+    const updatedGoal = await Goal.findByIdAndUpdate(req.params.id, req.body, {
+        new: true,
+    })
+
+    res.status(200).json(updatedGoal)
 })
 
 
@@ -35,7 +54,14 @@ const updateGoal = asyncHandler(async (req, res) => {
 // @route   DELETE /api/goals/:id
 // @access  Private
 const deleteGoal = asyncHandler(async (req, res) => {
-    res.status(200).json({message: `Delete goal ${req.params.id}`})
+    const goal = await Goal.findById(req.params.id)
+    if (!goal) {
+        res.status(400)
+        throw new Error('Goal not found')
+    }
+
+    await goal.remove()
+    res.status(200).json({ id: req.params.id })
 })
 
 

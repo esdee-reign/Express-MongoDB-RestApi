d713875eadf65543a264c97adc67563b54010303
Snehdeep Singh <snehdeepsingh221199@gmail.com>
2022-04-13 16:18:19 +0530

    frontend authentication & redux setup

--- frontend/src/app/store.js
+++ frontend/src/app/store.js
@@ -1,6 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit';
+import authSlice from '../features/auth/authSlice';
+import authReducer from '../features/auth/authSlice'
+
 
 export const store = configureStore({
   reducer: {
+    auth: authReducer
   },
 });

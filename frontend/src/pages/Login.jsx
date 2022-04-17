87a64dc2bc8d89d2d684e3c378489d7206d50e8b
Snehdeep Singh <snehdeepsingh221199@gmail.com>
2022-04-17 18:17:39 +0530

    update

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -4,7 +4,6 @@ import { useNavigate } from 'react-router-dom'
 import { toast } from 'react-toastify'
 import { login, reset } from '../features/auth/authSlice'
 import Spinner from '../components/Spinner'
-import MarqueeFooter from '../components/MarqueeFooter/MarqueeFooter';
 
 function Login() {
     const [formData, setFormData] = useState({
